382e86ba62284cf482823ac0b2daa89e20913820
vepetrelli <vepetrelli@gmail.com>
2022-09-17 12:11:49 -0300

    error maven

--- ProyectoIntegrador/backend/src/main/java/com/portfolio/portfolio/Security/Entity/UsuarioPrincipal.java
+++ ProyectoIntegrador/backend/src/main/java/com/portfolio/portfolio/Security/Entity/UsuarioPrincipal.java
@@ -1,44 +1,27 @@
 package com.portfolio.portfolio.Security.Entity;
 
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
-import java.util.Spliterator;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.IntFunction;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-import java.util.function.ToDoubleFunction;
-import java.util.function.ToIntFunction;
-import java.util.function.ToLongFunction;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 
 
+/**
+ *
+ * @author Usuario
+ */
+public class UsuarioPrincipal implements UserDetails {
 
-public class UsuarioPrincipal implements UserDetails{
     private String nombre;
     private String nombreUsuario;
     private String email;
     private String password;
-    private final Collection<? extends GrantedAuthority> authorities;
-    
-    //constructor
+    private Collection<? extends GrantedAuthority> authorities;
 
+    //Constructor
     public UsuarioPrincipal(String nombre, String nombreUsuario, String email, String password, Collection<? extends GrantedAuthority> authorities) {
         this.nombre = nombre;
         this.nombreUsuario = nombreUsuario;
@@ -46,9 +29,13 @@ public class UsuarioPrincipal implements UserDetails{
         this.password = password;
         this.authorities = authorities;
     }
-    public static UsuarioPrincipal build(Usuario usuario){
-    List<GrantedAuthority> authorities = usuario.getRoles().stream().map(rol -> new SimpleGrantedAuthority(rol.getRolNombre().name())).collect(Collectors.toList());
-    return new UsuarioPrincipal(usuario.getNombre(), usuario.getNombreUsuario(), usuario.getEmail(), usuario.getPassword(), authorities);
+
+    public static UsuarioPrincipal build(Usuario usuario) {
+        List<GrantedAuthority> authorities = usuario.getRoles().stream()
+                .map(rol -> new SimpleGrantedAuthority(rol.getRolNombre().name())).collect(Collectors
+                .toList());
+        return new UsuarioPrincipal(usuario.getNombre(), usuario.getNombreUsuario(), usuario.getEmail(),
+                 usuario.getPassword(), authorities);
     }
 
     @Override
@@ -60,13 +47,11 @@ public class UsuarioPrincipal implements UserDetails{
     public String getPassword() {
         return password;
     }
-    
-    
+
     public String getNombre() {
         return nombre;
     }
-    
-   
+
     public String getEmail() {
         return email;
     }
